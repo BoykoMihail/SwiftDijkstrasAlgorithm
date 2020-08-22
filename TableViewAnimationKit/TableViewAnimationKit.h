d0be512da087584cb8deef50837a57aa11d21ed7
Boyko Mihail <mikhail.boyko@touchin.ru>
2020-08-22 13:01:45 +0300

    Initial Commit

---
+++ TableViewAnimationKit/TableViewAnimationKit.h
@@ -0,0 +1,19 @@
+//
+//  TableViewAnimationKit.h
+//  TableViewAnimationKit
+//
+//  Created by Mikhail Boyko on 22.08.2020.
+//  Copyright © 2020 Mikhail Boyko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TableViewAnimationKit.
+FOUNDATION_EXPORT double TableViewAnimationKitVersionNumber;
+
+//! Project version string for TableViewAnimationKit.
+FOUNDATION_EXPORT const unsigned char TableViewAnimationKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TableViewAnimationKit/PublicHeader.h>
+
+
